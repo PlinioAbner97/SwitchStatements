c8217600957f2bc65b1343b9d4ebc3465d06a815
PlinioAbner <plinio414@gmail.com>
2022-03-11 10:30:26 -0500

    initial commit

---
+++ SwitchStatementsConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SwitchStatementsConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string subject = "Spanish";
+
+            Console.WriteLine("choose a subject");
+            string userSubject = Console.ReadLine();
+
+            switch (userSubject)
+            {
+                case "Spanish":
+                    Console.WriteLine("You are in the right class!");
+                    break;
+                case "English":
+                    Console.WriteLine("We don't do that here.");
+                    break;
+                case "Science":
+                    Console.WriteLine("We don't cut frogs here.");
+                    break;
+                case "Computers":
+                    Console.WriteLine("that class is tomorrow.");
+                    break;
+                default:
+                    Console.WriteLine("We teach spanish here.");
+                    break;
+            }
+        }
+    }
+}
